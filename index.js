0f521393202394d78441542a03432614cc9113a1
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-02 16:45:38 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+let currentUser="Paul";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage = welcomeMessage + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+excitedWelcomeMessage = excitedWelcomeMessage + currentUser.toLocaleUpperCase() + "!";
+
+let shortGreeting ="Welcome, ";
+
+shortGreeting = shortGreeting + currentUser.slice(0,1) + "!";
